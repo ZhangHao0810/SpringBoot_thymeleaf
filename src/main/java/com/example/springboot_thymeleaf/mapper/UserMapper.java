1c9018458ee4fa090cd0b25ff8c8243e405b0a3d
Super-Zhang <874993952@qq.com>
2023-04-11 14:09:00 +0800

    Initial commit

---
+++ src/main/java/com/example/springboot_thymeleaf/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.example.springboot_thymeleaf.mapper;
+
+import com.example.springboot_thymeleaf.entity.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
